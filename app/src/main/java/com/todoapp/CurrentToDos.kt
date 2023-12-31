013d3dbd622bfc427f8a69f6e4fa2a3c88eaf7f7
Hamed <hamed.naqibi@gmail.com>
2023-12-31 23:09:44 +0330

    initial commit

---
+++ app/src/main/java/com/todoapp/CurrentToDos.kt
@@ -0,0 +1,29 @@
+package com.todoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+
+class CurrentToDos : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_current_to_dos, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textView = view.findViewById<TextView>(R.id.textView)
+        textView.setOnClickListener{
+            Navigation.findNavController(textView).navigate(R.id.action_currentToDos_to_addTask)
+        }
+    }
+}
